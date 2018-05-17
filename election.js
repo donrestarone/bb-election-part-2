f12effbeb2c2fd9d9b05087bf27a552991a566b5
donrestarone <shashikejayatunge@gmail.com>
2018-05-17 17:11:56 -0400

    finish assignment

--- election.js
+++ election.js
@@ -17,6 +17,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 			var candidateNameLiTag = document.createElement('li');
 			var candidateVoteLiTag = document.createElement('li');
+	
+			candidateVoteLiTag.className = 'vote';
 
 			var name = candidate.name;
 			var votes = candidate.votes;
@@ -29,22 +31,61 @@ document.addEventListener("DOMContentLoaded", function() {
 			ul.append(candidateVoteLiTag);
 			ul.append(br);
 
+			//append form/button and the hidden field to the li
 			var form = document.createElement('form');
 			var submitButton = document.createElement('input');
 			submitButton.setAttribute('type', 'submit');
-			
+			//api call stuff
 			form.setAttribute('method', 'POST');
 			form.setAttribute('action', 'https://bb-election-api.herokuapp.com/vote');
 			
 			form.append(submitButton);
 			candidateVoteLiTag.append(form);
-			
+			//hidden field will have the name of the particular candidate
 			var hiddenField = document.createElement('input');
 			hiddenField.type = 'hidden';
 			hiddenField.name = 'name';
 			hiddenField.value = name;
 
 			form.append(hiddenField);
+
+			//make the ajax side of things work
+			form.addEventListener('submit', function(event){
+				console.log('ive been clicked');
+				event.preventDefault();
+				$.ajax({
+					url: form.getAttribute('action'),
+					method: form.getAttribute('method'),
+					dataType: 'json',
+					data: $(hiddenField).serialize()
+				}).done(function(data){
+					console.log('request successful');
+
+					var request = $.ajax({
+						url: url,
+						method: 'GET',
+						dataType: 'json',
+					}).done(function(data){
+						console.log(data);
+						var updatedCandidates = data['candidates'];
+						var i = 0;
+
+						var candidateLiListVote = document.querySelectorAll('.vote')
+
+						updatedCandidates.forEach(function(candidate){
+							votes = candidate.votes;
+							candidateLiListVote[i].innerText = "";
+							candidateLiListVote[i].innerText = "votes: " + votes;
+							i++;
+
+							
+						})
+					})
+
+				}).fail(function(jqXHR, status){
+					console.log('fail' + jqXHR + status);
+				})
+			})
 
 		})
 		
